dcb90a187be7314cf5fb84d57c5fd4a9a2f3bbf5
GraniteMask <ratnadeepastronomy@gmail.com>
2021-08-03 03:48:28 +0530

    refresh issue fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(()=>{
+    const jssStyles = document.querySelector('#jss-server-side')
+    if(jssStyles){
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  },[]) //written to solve the problem of not applying material styling after refresh
+
   return <Component {...pageProps} />
 }
 
