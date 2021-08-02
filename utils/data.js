b11878fe4a5db22af2d09cc004ee1028bfdead98
GraniteMask <ratnadeepastronomy@gmail.com>
2021-08-03 04:00:24 +0530

    data page added

---
+++ utils/data.js
@@ -0,0 +1,39 @@
+const data ={
+    products:[
+        {
+            name: 'Wallpaper 1',
+            category: 'Abstract',
+            image: '/images/sample.jpg',
+            price: 100,
+            brand: 'Free',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A popular shirt',
+        },
+        {
+            name: 'Wallpaper 2',
+            category: 'Abstract',
+            image: '/images/sample.jpg',
+            price: 100,
+            brand: 'Free',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A popular shirt',
+        },
+        {
+            name: 'Wallpaper 3',
+            category: 'Abstract',
+            image: '/images/sample.jpg',
+            price: 100,
+            brand: 'Free',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A popular shirt',
+        },
+    ]
+}
+
+export default data
