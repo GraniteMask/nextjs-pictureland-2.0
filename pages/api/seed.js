deb9d7d7e20de7508bfacf59472a7a93ad792540
GraniteMask <ratnadeepastronomy@gmail.com>
2021-08-04 21:49:15 +0530

    seed api added

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,16 +1,18 @@
 import nc from 'next-connect'
-import Product from '../../../models/Product'
-import db from '../../../utils/db'
+import Product from '../../models/Product'
+import db from '../../utils/db'
+import data from '../../utils/data'
 
 const handler = nc()
 
 handler.get(async(req, res)=>{
     await db.connect()
 
-    const products = await Product.find({})
+    await Product.deleteMany();
+    await Product.insertMany(data.products);
     await db.disconnect()
 
-    res.send(products)
+    res.send({message: 'seeded successfully'})
 })
 
 export default handler
