64d69073fe286ca240639adcd283d7cf9d888dd9
GraniteMask <ratnadeepastronomy@gmail.com>
2021-08-07 00:27:44 +0530

    placeorder screen layout added

--- pages/placeorder.js
+++ pages/placeorder.js
@@ -7,11 +7,13 @@ import Image from 'next/image'
 import dynamic from 'next/dynamic'
 import axios from 'axios'
 import { useRouter } from 'next/router'
+import useStyles from '../utils/styles'
 
 function PlaceOrder() {
+    const classes = useStyles()
     const router = useRouter()
     const {state, dispatch} = useContext(Store)
-    const { cart: {cartItems} } = state
+    const { cart: {cartItems, shippingAddress, paymentMethod} } = state
 
     return (
         <Layout title="Shopping Cart">
@@ -19,59 +21,133 @@ function PlaceOrder() {
            
                 <Grid container spacing={1}>
                     <Grid item md={9} xs={12}>
-                        <TableContainer>
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Image</TableCell>
-                                        <TableCell>Name</TableCell>
-                                        <TableCell align="right">Quantity</TableCell>
-                                        <TableCell align="right">Price</TableCell>
-                                    </TableRow>
-                                </TableHead>
 
-                                <TableBody>
-                                    {cartItems.map((item)=>(
-                                        <TableRow key={item._id}>
+                        <Card className={classes.section}>
+                                <List>
+                                    <ListItem>
+                                        <Typography component="h2" varaint="h2">Shipping Address</Typography>
+                                    </ListItem>
+                                    <ListItem>
+                                        {shippingAddress.fullName}, {shippingAddress.address},{' '}{shippingAddress.city}, {shippingAddress.postalCode},{' '}{shippingAddress.country}
+                                    </ListItem>
+                                </List>
+                        </Card>
 
-                                            <TableCell>
-                                                <NextLink href={`/product/${item.slug}`} passHref>
-                                                    <Link>
-                                                        <Image src={item.image} alt={item.name} width={50} height={50}></Image>
-                                                    </Link>
-                                                </NextLink>
-                                            </TableCell>
+                        <Card className={classes.section}>
+                                <List>
+                                    <ListItem>
+                                        <Typography component="h2" varaint="h2">Payment Method</Typography>
+                                    </ListItem>
+                                    <ListItem>
+                                        {paymentMethod}
+                                    </ListItem>
+                                </List>
+                        </Card>
 
-                                            <TableCell>
-                                                <NextLink href={`/product/${item.slug}`} passHref>
-                                                    <Link>
-                                                        <Typography>{item.name}</Typography>
-                                                    </Link>
-                                                </NextLink>
-                                            </TableCell>
+                        <Card className={classes.section}>
+                            <List>
+                                <ListItem>
+                                    <Typography component="h2" varaint="h2">Order Items</Typography>
+                                </ListItem>
+                                <ListItem>
+                                <TableContainer>
+                                    <Table>
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>Image</TableCell>
+                                                <TableCell>Name</TableCell>
+                                                <TableCell align="right">Quantity</TableCell>
+                                                <TableCell align="right">Price</TableCell>
+                                            </TableRow>
+                                        </TableHead>
 
-                                            <TableCell align="right">
-                                                <Typography>{item.quantity}</Typography>
-                                            </TableCell>
+                                        <TableBody>
+                                            {cartItems.map((item)=>(
+                                                <TableRow key={item._id}>
 
-                                            <TableCell align="right">
-                                                <Typography>${item.price}</Typography>
-                                            </TableCell>
+                                                    <TableCell>
+                                                        <NextLink href={`/product/${item.slug}`} passHref>
+                                                            <Link>
+                                                                <Image src={item.image} alt={item.name} width={50} height={50}></Image>
+                                                            </Link>
+                                                        </NextLink>
+                                                    </TableCell>
 
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
+                                                    <TableCell>
+                                                        <NextLink href={`/product/${item.slug}`} passHref>
+                                                            <Link>
+                                                                <Typography>{item.name}</Typography>
+                                                            </Link>
+                                                        </NextLink>
+                                                    </TableCell>
 
-                        </TableContainer>
+                                                    <TableCell align="right">
+                                                        <Typography>{item.quantity}</Typography>
+                                                    </TableCell>
+
+                                                    <TableCell align="right">
+                                                        <Typography>${item.price}</Typography>
+                                                    </TableCell>
+
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+
+                                </TableContainer> 
+                                </ListItem>
+                            </List>
+ 
+                        </Card>
+                       
                     </Grid>
                     <Grid item md={3} xs={12}>
-                        <Card>
+                        <Card className={classes.section}>
                             <List>
                                 <ListItem>
                                     <Typography variant="h2">
                                        Order Summary
                                     </Typography>
+                                </ListItem>
+                                <ListItem>
+                                    <Grid container>
+                                        <Grid item xs={6}>
+                                            <Typography>Items:</Typography>
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <Typography>${itemsPrice}</Typography>
+                                        </Grid>
+                                    </Grid>
+                                </ListItem>
+                                <ListItem>
+                                    <Grid container>
+                                        <Grid item xs={6}>
+                                            <Typography>Tax:</Typography>
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <Typography>${taxPrice}</Typography>
+                                        </Grid>
+                                    </Grid>
+                                </ListItem>
+                                <ListItem>
+                                    <Grid container>
+                                        <Grid item xs={6}>
+                                            <Typography>Shipping:</Typography>
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <Typography>${shippingPrice}</Typography>
+                                        </Grid>
+                                    </Grid>
+                                </ListItem>
+                                <ListItem>
+                                    <Grid container>
+                                        <Grid item xs={6}>
+                                            <Typography><strong>Total:</strong></Typography>
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <Typography><strong>${totalPrice}</strong></Typography>
+                                        </Grid>
+                                    </Grid>
                                 </ListItem>
                                 <ListItem>
                                     <Button variant="contained" color="primary" fullWidth>Place your Order</Button>
--- utils/Store.js
+++ utils/Store.js
@@ -7,6 +7,7 @@ const initialState = {
     cart:{
         cartItems: Cookies.get('cartItems') ? JSON.parse(Cookies.get('cartItems')) : [],
         shippingAddress: Cookies.get('shippingAddress') ? JSON.parse(Cookies.get('shippingAddress')) : {},
+        paymentMethod: Cookies.get('paymentMethod') ? Cookies.get('paymentMethod') : '',
     },
 
     userInfo: Cookies.get('userInfo') ? JSON.parse(Cookies.get('userInfo')) : null,
