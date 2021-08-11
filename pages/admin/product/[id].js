34b29312ce15823f075c9ba4c4e466b2d20c5d55
GraniteMask <ratnadeepastronomy@gmail.com>
2021-08-12 01:14:23 +0530

    product update screen completed

---
+++ pages/admin/product/[id].js
@@ -0,0 +1,283 @@
+import React from 'react'
+import dynamic from 'next/dynamic'
+import { useContext } from 'react'
+import { Store } from '../utils/Store'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useReducer } from 'react'
+import axios from 'axios'
+import { getError } from '../utils/error'
+import Layout from '../components/layout'
+import { Button, Card, CircularProgress, Grid, ListItem, ListItemText, TableBody, TableCell, TableContainer, Typography, List, Table, TableHead, TableRow, TextField } from '@material-ui/core'
+import useStyles from '../utils/styles'
+import NextLink from 'next/link'
+import { Controller, useForm } from 'react-hook-form'
+import { useSnackbar } from 'notistack'
+import Cookies from 'js-cookie'
+
+function reducer(state, action){
+    switch (action.type){
+        case 'FETCH_REQUEST':
+            return {...state, loading: true, error: ''}
+        case 'FETCH_SUCCESS':
+            return {...state, loading: false, error:''}
+        case 'FETCH_FAIL':
+            return {...state, loading: false, error: action.payload}
+        default:
+            return state
+    }
+}
+
+function ProductEdit({params}) {
+    const productId = params.id
+    const {state, dispatch} = useContext(Store)
+    const {handleSubmit, control, formState: {errors}, setValue} = useForm()
+    const [{loading, error}, dispatch] = useReducer(reducer, {loading: true, error:''})
+    const { userInfo } = state
+    const router = useRouter()
+    const classes = useStyles()
+    const {enqueueSnackbar, closeSnackbar} = useSnackbar()
+
+    useEffect(()=>{
+        if(!userInfo){
+            return router.push('/login')
+        }else{
+            const fetchData = async () =>{
+                try{
+                    dispatch({type: 'FETCH_REQUEST'})
+                    const {data} = await axios.get(`/api/admin/products/${productId}`,{
+                        headers: {authorization: `Bearer ${userInfo.token}`}
+                    })
+                    dispatch({type: 'FETCH_SUCCESS'})
+                    setValue('name', userInfo.name)
+                    setValue('slug', userInfo.slug)
+                    setValue('price', userInfo.price)
+                    setValue('image', userInfo.image)
+                    setValue('category', userInfo.category)
+                    setValue('brand', userInfo.brand)
+                    setValue('countInStock', userInfo.countInStock)
+                    setValue('description', userInfo.description)
+                }catch(err){
+                    dispatch({type: 'FETCH_FAIL', payload: getError(err)})
+                } 
+            }
+            fetchData()
+        }
+        
+    }, [])
+
+    const submitHandler = async ({name}) =>{
+        // e.preventDefault()
+        closeSnackbar()
+
+        try{
+            const {data} = await axios.put(`/api/admin/products/${productId}`, {name},{
+                headers: 
+                {authorization: `Bearer ${userInfo.token}`}
+            })
+            
+            enqueueSnackbar("Product updated successfully", {variant: 'success'})
+        }catch(err){
+            enqueueSnackbar(err.response.data ? err.response.data.message : err.message, {variant: 'error'})
+            // alert(err.response.data ? err.response.data.message : err.message)
+        }
+        
+    }
+
+    return (
+        <Layout title='Edit Product'>
+             <Grid container spacing={1}>
+                <Grid item md={3} xs={12}>
+                    <Card className={classes.section}>
+                        <List>
+                            <NextLink  href="/admin/dashboard" passHref>
+                                <ListItem button component="a">
+                                    <ListItemText primary="Admin Dashboard"></ListItemText>
+                                </ListItem>
+                            </NextLink>
+                            <NextLink href="/admin/orders" passHref>
+                                <ListItem  button component="a">
+                                    <ListItemText primary="Orders"></ListItemText>
+                                </ListItem>
+                            </NextLink>
+                            <NextLink href="/admin/products" passHref>
+                                <ListItem selected button component="a">
+                                    <ListItemText primary="Products"></ListItemText>
+                                </ListItem>
+                            </NextLink>
+                        </List>
+                    </Card>
+                </Grid>
+                <Grid item md={9} xs={12}>
+                    <Card className={classes.section}>
+                        <List>
+                            <ListItem>
+                                <Typography component="h1" variant="h1">
+                                    Edit Product of {productId}
+                                </Typography>
+                            </ListItem>
+                            <ListItem>
+                                {loading && <CircularProgress></CircularProgress>}
+                                {error && (
+                                    <Typography className={classes.error}>{error}</Typography>
+                                )}
+                            </ListItem>
+                            <ListItem>
+                            <form onSubmit={handleSubmit(submitHandler)} className={classes.form}>
+                                <List>
+
+                                    <ListItem>
+                                        <Controller name="name" control={control} defaultValue="" rules={{
+                                            required: true,
+                                           
+                                        }} render={({field})=>(
+                                            <TextField variant="outlined" fullWidth id="name" label="Name" 
+                                            error={Boolean(errors.name)}
+                                            helperText ={
+                                                errors.name ? 
+                                                'Name is required'
+                                                :''}
+                                            {...field}></TextField>
+                                        )}>
+
+                                        </Controller>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Controller name="slug" control={control} defaultValue="" rules={{
+                                            required: true,
+                                           
+                                        }} render={({field})=>(
+                                            <TextField variant="outlined" fullWidth id="slug" label="Slug" 
+                                            error={Boolean(errors.slug)}
+                                            helperText ={
+                                                errors.slug ? 
+                                                'Slug is required'
+                                                :''}
+                                            {...field}></TextField>
+                                        )}>
+
+                                        </Controller>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Controller name="price" control={control} defaultValue="" rules={{
+                                            required: true,
+                                           
+                                        }} render={({field})=>(
+                                            <TextField variant="outlined" fullWidth id="price" label="Price" 
+                                            error={Boolean(errors.price)}
+                                            helperText ={
+                                                errors.price ? 
+                                                'Price is required'
+                                                :''}
+                                            {...field}></TextField>
+                                        )}>
+
+                                        </Controller>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Controller name="image" control={control} defaultValue="" rules={{
+                                            required: true,
+                                           
+                                        }} render={({field})=>(
+                                            <TextField variant="outlined" fullWidth id="image" label="Image" 
+                                            error={Boolean(errors.image)}
+                                            helperText ={
+                                                errors.image ? 
+                                                'Image is required'
+                                                :''}
+                                            {...field}></TextField>
+                                        )}>
+
+                                        </Controller>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Controller name="category" control={control} defaultValue="" rules={{
+                                            required: true,
+                                           
+                                        }} render={({field})=>(
+                                            <TextField variant="outlined" fullWidth id="category" label="Category" 
+                                            error={Boolean(errors.category)}
+                                            helperText ={
+                                                errors.category ? 
+                                                'Category is required'
+                                                :''}
+                                            {...field}></TextField>
+                                        )}>
+
+                                        </Controller>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Controller name="brand" control={control} defaultValue="" rules={{
+                                            required: true,
+                                           
+                                        }} render={({field})=>(
+                                            <TextField variant="outlined" fullWidth id="brand" label="Brand" 
+                                            error={Boolean(errors.brand)}
+                                            helperText ={
+                                                errors.brand ? 
+                                                'Brand is required'
+                                                :''}
+                                            {...field}></TextField>
+                                        )}>
+
+                                        </Controller>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Controller name="countInStock" control={control} defaultValue="" rules={{
+                                            required: true,
+                                           
+                                        }} render={({field})=>(
+                                            <TextField variant="outlined" fullWidth id="countInStock" label="Count In Stock" 
+                                            error={Boolean(errors.countInStock)}
+                                            helperText ={
+                                                errors.countInStock ? 
+                                                'Count In Stock is required'
+                                                :''}
+                                            {...field}></TextField>
+                                        )}>
+
+                                        </Controller>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Controller name="description" control={control} defaultValue="" rules={{
+                                            required: true,
+                                           
+                                        }} render={({field})=>(
+                                            <TextField variant="outlined" fullWidth multiline id="description" label="Description" 
+                                            error={Boolean(errors.description)}
+                                            helperText ={
+                                                errors.description ? 
+                                                'Description is required'
+                                                :''}
+                                            {...field}></TextField>
+                                        )}>
+
+                                        </Controller>
+                                    </ListItem>
+                                   
+                                    <ListItem>
+                                        <Button variant="contained" type="submit" fullWidth color="primary">Update</Button>
+                                    </ListItem>
+                                </List>
+                            </form>
+                            </ListItem>
+                        </List>
+                    </Card>
+                </Grid>
+            </Grid>
+            
+               
+           
+
+            
+        </Layout>
+    )
+}
+
+export async function getServerSideProps({params}){
+    return{
+        props: {params}
+    }
+}
+
+export default dynamic(()=> Promise.resolve(ProductEdit),{ssr:false})
