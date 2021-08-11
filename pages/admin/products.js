6fd5b8c835cbe0eff67374a7e15ad44e5670ddcc
GraniteMask <ratnadeepastronomy@gmail.com>
2021-08-11 21:53:36 +0530

    products page and api completed

---
+++ pages/admin/products.js
@@ -0,0 +1,139 @@
+import React from 'react'
+import dynamic from 'next/dynamic'
+import { useContext } from 'react'
+import { Store } from '../../utils/Store'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useReducer } from 'react'
+import axios from 'axios'
+import { getError } from '../../utils/error'
+import Layout from '../../components/layout'
+import { Button, Card, CircularProgress, Grid, ListItem, ListItemText, TableBody, TableCell, TableContainer, Typography, List, Table, TableHead, TableRow, CardContent, CardActions } from '@material-ui/core'
+import useStyles from '../../utils/styles'
+import NextLink from 'next/link'
+
+function reducer(state, action){
+    switch(action.type){
+        case "FETCH_REQUEST":
+            return {...state, loading: true, error:''}
+        case 'FETCH_SUCCESS':
+            return {...state, loading: false, products: action.payload, error:''}
+        case 'FETCH_FAIL':
+            return {...state, loading: false, error: action.payload}
+
+        default:
+            state
+    }
+}
+
+function Products() {
+    const {state} = useContext(Store)
+    const { userInfo } = state
+    const router = useRouter()
+    const classes = useStyles()
+
+    const [{loading, error, products}, dispatch] = useReducer(reducer, {loading: true, products:[], error:''})
+
+    useEffect(()=>{
+        if(!userInfo){
+            router.push('/login')
+        }
+        const fetchData = async () =>{
+            try{
+                dispatch({type: 'FETCH_REQUEST'})
+                const {data} = await axios.get(`/api/admin/products`,{
+                    headers: {authorization: `Bearer ${userInfo.token}`}
+                })
+                dispatch({type:'FETCH_SUCCESS', payload:data})
+            }catch(err){
+                dispatch({type:'FETCH_FAIL', payload:getError(err)})
+            }
+        }
+        fetchData()
+    }, [])
+    return (
+        <Layout title='Your Product History'>
+             <Grid container spacing={1}>
+                <Grid item md={3} xs={12}>
+                    <Card className={classes.section}>
+                        <List>
+                            <NextLink  href="/admin/dashboard" passHref>
+                                <ListItem  button component="a">
+                                    <ListItemText primary="Admin Dashboard"></ListItemText>
+                                </ListItem>
+                            </NextLink>
+                            <NextLink href="/admin/products" passHref>
+                                <ListItem selected button component="a">
+                                    <ListItemText primary="Products"></ListItemText>
+                                </ListItem>
+                            </NextLink>
+                        </List>
+                    </Card>
+                </Grid>
+                <Grid item md={9} xs={12}>
+                    <Card className={classes.section}>
+                        <List>
+                            <ListItem>
+                                <Typography component="h1" variant="h1">
+                                    Products
+                                </Typography>
+                            </ListItem>
+                            
+                            <ListItem>
+                            {loading ? (<CircularProgress />)
+                            :
+                            error ? (<Typography className={classes.error}>{error}</Typography>)
+                            :(
+                                <TableContainer>
+                                    <Table>
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>ID</TableCell>
+                                                <TableCell>USER</TableCell>
+                                                <TableCell>DATE</TableCell>
+                                                <TableCell>TOTAL</TableCell>
+                                                <TableCell>PAID</TableCell>
+                                                <TableCell>DELIVERED</TableCell>
+                                                <TableCell>ACTION</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {products.map((product)=>(
+                                                <TableRow key={product._id}>
+                                                    <TableCell>{product._id.substring(20, 24)}</TableCell>
+                                                    <TableCell>{product.user ? product.user.name : 'DELETED USER'}</TableCell>
+                                                    <TableCell>{product.createdAt}</TableCell>
+                                                    <TableCell>${product.totalPrice}</TableCell>
+                                                    <TableCell>{product.isPaid ?
+                                                    `paid at ${product.paidAt}` :
+                                                    'not paid'}</TableCell>
+                                                    <TableCell>{product.iDelivered ?
+                                                    `delivered at ${product.deliveredAt}` :
+                                                    'not delivered'}</TableCell>
+                                                    <TableCell>
+                                                        <NextLink href={`/product/${product._id}`} passHref>
+                                                            <Button variant="contained">Details</Button>
+                                                        </NextLink>
+                                                    </TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            )}
+                            </ListItem>
+                            
+                        </List>
+                    </Card>
+                </Grid>
+            </Grid>
+            
+               
+           
+
+            
+        </Layout>
+    )
+}
+
+export default dynamic(()=> Promise.resolve(Products),{ssr:false})
---
+++ pages/api/admin/products.js
@@ -0,0 +1,22 @@
+import nc from 'next-connect'
+import Product from '../../../models/Product'
+import Product from '../../../models/Product'
+import User from '../../../models/User'
+import { isAuth, isAdmin } from '../../../utils/auth'
+import db from '../../../utils/db'
+import {onError} from '../../../utils/error'
+
+const handler = nc({
+    onError
+})
+
+handler.use(isAuth, isAdmin)
+
+handler.get(async(req,res)=>{
+    await db.connect()
+    const products = await Product.find({})
+    await db.disconnect()
+    res.send(products)
+})
+
+export default handler
