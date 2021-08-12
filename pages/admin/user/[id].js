1d1be7e0f9b8f167ba1afd93ab26438905353487
GraniteMask <ratnadeepastronomy@gmail.com>
2021-08-12 22:36:55 +0530

    edit users bt admin

---
+++ pages/admin/user/[id].js
@@ -0,0 +1,205 @@
+import React, { useState } from 'react'
+import dynamic from 'next/dynamic'
+import { useContext } from 'react'
+import { Store } from '../../../utils/Store'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useReducer } from 'react'
+import axios from 'axios'
+import { getError } from '../../../utils/error'
+import Layout from '../../../components/layout'
+import { Button, Card, CircularProgress, Grid, ListItem, ListItemText, TableBody, TableCell, TableContainer, Typography, List, Table, TableHead, TableRow, TextField, FormControlLabel, Checkbox } from '@material-ui/core'
+import useStyles from '../../../utils/styles'
+import NextLink from 'next/link'
+import { Controller, useForm } from 'react-hook-form'
+import { useSnackbar } from 'notistack'
+
+
+function reducer(state, action){
+    switch (action.type){
+        case 'FETCH_REQUEST':
+            return {...state, loading: true, error: ''}
+        case 'FETCH_SUCCESS':
+            return {...state, loading: false, error:''}
+        case 'FETCH_FAIL':
+            return {...state, loading: false, error: action.payload}
+        case 'UPDATE_REQUEST':
+            return {...state, loadingUpdate: true, errorUpdate: ''}
+        case 'UPDATE_SUCCESS':
+            return {...state, loadingUpdate: false, errorUpdate:''}
+        case 'UPDATE_FAIL':
+            return {...state, loadingUpdate: false, errorUpdate: action.payload}
+        case 'UPLOAD_REQUEST':
+            return {...state, loadingUpload: true, errorUpload:''}
+        case 'UPLOAD_SUCCESS':
+            return{
+                ...state,
+                loadingUpload: false,
+                errorUpload: ''
+            }
+        case 'UPLOAD_FAIL':
+            return {...state, loadingUpload: false, errorUpload: action.payload}
+        default:
+            return state
+    }
+}
+
+function UserEdit({params}) {
+    const userId = params.id
+    const {state} = useContext(Store)
+    const {handleSubmit, control, formState: {errors}, setValue} = useForm()
+    const [isAdmin, setIsAdmin] = useState(false)
+    const [{loading, error, loadingUpdate, loadingUpload}, dispatch] = useReducer(reducer, {loading: true, error:''})
+    const { userInfo } = state
+    const router = useRouter()
+    const classes = useStyles()
+    const {enqueueSnackbar, closeSnackbar} = useSnackbar()
+
+    useEffect(()=>{
+        if(!userInfo){
+            return router.push('/login')
+        }else{
+            const fetchData = async () =>{
+                try{
+                    dispatch({type: 'FETCH_REQUEST'})
+                    const {data} = await axios.get(`/api/admin/users/${userId}`,{
+                        headers: {authorization: `Bearer ${userInfo.token}`}
+                    })
+                    setIsAdmin(data.isAdmin)
+                    dispatch({type: 'FETCH_SUCCESS'})
+                    setValue('name', data.name)
+                    
+                }catch(err){
+                    dispatch({type: 'FETCH_FAIL', payload: getError(err)})
+                } 
+            }
+            fetchData()
+        }
+        
+    }, [])
+
+    const submitHandler = async ({name}) =>{
+        // e.preventDefault()
+        closeSnackbar()
+
+        try{
+            dispatch({type: 'UPDATE_REQUEST'})
+            await axios.put(`/api/admin/users/${userId}`, {name,isAdmin},{
+                headers: 
+                {authorization: `Bearer ${userInfo.token}`}
+            })
+            dispatch({type: 'UPDATE_SUCCESS'})
+            enqueueSnackbar("User updated successfully", {variant: 'success'})
+            router.push('/admin/users')
+        }catch(err){
+            dispatch({type: 'UPDATE_FAIL', payload: getError(err)})
+            enqueueSnackbar(err.response.data ? err.response.data.message : err.message, {variant: 'error'})
+            // alert(err.response.data ? err.response.data.message : err.message)
+        }
+        
+    }
+
+    return (
+        <Layout title='Edit User'>
+             <Grid container spacing={1}>
+                <Grid item md={3} xs={12}>
+                    <Card className={classes.section}>
+                        <List>
+                            <NextLink  href="/admin/dashboard" passHref>
+                                <ListItem button component="a">
+                                    <ListItemText primary="Admin Dashboard"></ListItemText>
+                                </ListItem>
+                            </NextLink>
+                            <NextLink href="/admin/orders" passHref>
+                                <ListItem  button component="a">
+                                    <ListItemText primary="Orders"></ListItemText>
+                                </ListItem>
+                            </NextLink>
+                            <NextLink href="/admin/products" passHref>
+                                <ListItem button component="a">
+                                    <ListItemText primary="Products"></ListItemText>
+                                </ListItem>
+                            </NextLink>
+                            <NextLink href="/admin/users" passHref>
+                                <ListItem selected button component="a">
+                                    <ListItemText primary="Users"></ListItemText>
+                                </ListItem>
+                            </NextLink>
+                        </List>
+                    </Card>
+                </Grid>
+                <Grid item md={9} xs={12}>
+                    <Card className={classes.section}>
+                        <List>
+                            <ListItem>
+                                <Typography component="h1" variant="h1">
+                                    Edit User of {userId}
+                                </Typography>
+                            </ListItem>
+                            <ListItem>
+                                {loading && <CircularProgress></CircularProgress>}
+                                {error && (
+                                    <Typography className={classes.error}>{error}</Typography>
+                                )}
+                            </ListItem>
+                            <ListItem>
+                            <form onSubmit={handleSubmit(submitHandler)} className={classes.form}>
+                                <List>
+
+                                    <ListItem>
+                                        <Controller name="name" control={control} defaultValue="" rules={{
+                                            required: true,
+                                           
+                                        }} render={({field})=>(
+                                            <TextField variant="outlined" fullWidth id="name" label="Name" 
+                                            error={Boolean(errors.name)}
+                                            helperText ={
+                                                errors.name ? 
+                                                'Name is required'
+                                                :''}
+                                            {...field}></TextField>
+                                        )}>
+
+                                        </Controller>
+                                    </ListItem>
+
+                                    <ListItem>
+                                        <FormControlLabel
+                                            label="Is Admin"
+                                            control={
+                                            <Checkbox
+                                                onClick={(e) => setIsAdmin(e.target.checked)}
+                                                checked={isAdmin}
+                                                name="isAdmin"
+                                            />
+                                            }
+                                        ></FormControlLabel>
+                                    </ListItem>
+
+                                    <ListItem>
+                                        <Button variant="contained" type="submit" fullWidth color="primary">Update</Button>
+                                        {loadingUpdate && <CircularProgress />}
+                                    </ListItem>
+                                </List>
+                            </form>
+                            </ListItem>
+                        </List>
+                    </Card>
+                </Grid>
+            </Grid>
+            
+               
+           
+
+            
+        </Layout>
+    )
+}
+
+export async function getServerSideProps({params}){
+    return{
+        props: {params}
+    }
+}
+
+export default dynamic(()=> Promise.resolve(UserEdit),{ssr:false})
