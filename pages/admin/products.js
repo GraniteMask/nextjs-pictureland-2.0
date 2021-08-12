27ac3f58d9eeef70f63ed13bdeed2c0a528cadfb
GraniteMask <ratnadeepastronomy@gmail.com>
2021-08-12 21:42:41 +0530

    delete product

--- pages/admin/products.js
+++ pages/admin/products.js
@@ -11,6 +11,7 @@ import Layout from '../../components/layout'
 import { Button, Card, CircularProgress, Grid, ListItem, ListItemText, TableBody, TableCell, TableContainer, Typography, List, Table, TableHead, TableRow, CardContent, CardActions } from '@material-ui/core'
 import useStyles from '../../utils/styles'
 import NextLink from 'next/link'
+import { useSnackbar } from 'notistack'
 
 function reducer(state, action){
     switch(action.type){
@@ -20,7 +21,20 @@ function reducer(state, action){
             return {...state, loading: false, products: action.payload, error:''}
         case 'FETCH_FAIL':
             return {...state, loading: false, error: action.payload}
-
+        case 'CREATE_REQUEST':
+            return {...state, loadingCreate: false}
+        case 'CREATE_SUCCESS':
+            return {...state, loadingCreate: false}
+        case 'CREATE_FAIL':
+            return {...state, loadingCreate: false}
+        case 'DELETE_REQUEST':
+            return {...state, loadingDelete: true}
+        case 'DELETE_SUCCESS':
+            return {...state, loadingDelete: false, successDelete: true}
+        case 'DELETE_FAIL':
+            return {...state, loadingDelete: false}
+        case 'DELETE_RESET':
+            return {...state, loadingDelete: false, successDelete: false}
         default:
             state
     }
@@ -31,8 +45,10 @@ function Products() {
     const { userInfo } = state
     const router = useRouter()
     const classes = useStyles()
+    const {enqueueSnackbar, closeSnackbar} = useSnackbar()
 
-    const [{loading, error, products}, dispatch] = useReducer(reducer, {loading: true, products:[], error:''})
+
+    const [{loading, error, products, loadingCreate, successDelete, loadingDelete}, dispatch] = useReducer(reducer, {loading: true, products:[], error:''})
 
     useEffect(()=>{
         if(!userInfo){
@@ -49,8 +65,56 @@ function Products() {
                 dispatch({type:'FETCH_FAIL', payload:getError(err)})
             }
         }
-        fetchData()
-    }, [])
+        if(successDelete){
+            dispatch({type: 'DELETE_RESET'})
+        }else{
+            fetchData()
+        }
+        
+    }, [successDelete])
+
+    const createHandler = async() =>{
+        if(!window.confirm('Are you sure?')){
+            return
+        }
+        try{
+            dispatch({type: 'CREATE_REQUEST'})
+            const {data} = await axios.post(
+                `/api/admin/products`,
+                {},
+                {
+                    headers: {authorization: `Bearer ${userInfo.token}`}
+                }
+            )
+            dispatch({type: 'CREATE_SUCCESS'})
+            enqueueSnackbar("Product created successfully", {variant: 'success'})
+            router.push(`/admin/product/${data.product._id}`)
+        }catch(err){
+            dispatch({type: 'CREATE_FAIL'})
+            enqueueSnackbar(err.response.data ? err.response.data.message : err.message, {variant: 'error'})
+        }
+    }
+
+    const deleteHandler = async(productId) =>{
+        if(!window.confirm('Are you sure?')){
+            return
+        }
+        try{
+            dispatch({type: 'DELETE_REQUEST'})
+            await axios.delete(
+                `/api/admin/products/${productId}`,
+                {
+                    headers: {authorization: `Bearer ${userInfo.token}`}
+                }
+            )
+            dispatch({type: 'DELETE_SUCCESS'})
+            enqueueSnackbar("Product deleted successfully", {variant: 'success'})
+        }catch(err){
+            dispatch({type: 'DELETE_FAIL'})
+            enqueueSnackbar(err.response.data ? err.response.data.message : err.message, {variant: 'error'})
+        }
+    }
+
     return (
         <Layout title='Your Product History'>
              <Grid container spacing={1}>
@@ -79,9 +143,21 @@ function Products() {
                     <Card className={classes.section}>
                         <List>
                             <ListItem>
-                                <Typography component="h1" variant="h1">
-                                    Products
-                                </Typography>
+                                <Grid container alignItems="center">
+                                    <Grid item xs={6}>
+                                        <Typography component="h1" variant="h1">
+                                            Products
+                                        </Typography>
+                                        {loadingDelete && <CircularProgress />}
+                                    </Grid>
+                                    <Grid align="right" item xs={6}>
+                                        <Button onClick={createHandler} color="primary" variant="contained">
+                                            Create
+                                        </Button>
+                                        {loadingCreate && <CircularProgress/>}
+                                    </Grid>
+                                </Grid>
+                                
                             </ListItem>
                             
                             <ListItem>
@@ -115,7 +191,7 @@ function Products() {
                                                         <NextLink href={`/admin/product/${product._id}`} passHref>
                                                             <Button size="small" variant="contained">Edit</Button>
                                                         </NextLink>{' '}
-                                                        <Button size="small" variant="contained">Delete</Button>
+                                                        <Button size="small" variant="contained" onClick={()=>deleteHandler(product._id)}>Delete</Button>
                                                     </TableCell>
                                                 </TableRow>
                                             ))}
---
+++ pages/api/admin/products/index.js
@@ -0,0 +1,36 @@
+import nc from 'next-connect'
+import Product from '../../../../models/Product'
+import { isAuth, isAdmin } from '../../../../utils/auth'
+import db from '../../../../utils/db'
+
+const handler = nc()
+handler.use(isAuth, isAdmin)
+
+handler.get(async(req,res)=>{
+    await db.connect()
+    const products = await Product.find({})
+    await db.disconnect()
+    res.send(products)
+})
+
+handler.post(async(req,res)=>{
+    await db.connect()
+    const newProduct = new Product({
+        name: 'sample name',
+    slug: 'sample-slug-' + Math.random(),
+    image: '',
+    price: 0,
+    category: 'sample category',
+    brand: 'sample brand',
+    countInStock: 0,
+    description: 'sample description',
+    rating: 0,
+    numReviews: 0,
+  });
+
+  const product = await newProduct.save();
+  await db.disconnect();
+  res.send({ message: 'Product Created', product });
+});
+
+export default handler;
